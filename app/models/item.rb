70bb9396177edf9d9d98df0402493cd118b9636a
chayadeaver <chayadeaver@gmail.com>
2021-01-12 16:16:21 -0500

    add validations to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :menu, optional: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  
 end
