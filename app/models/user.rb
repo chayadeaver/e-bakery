91e79ffcc51440fbb5e7b46d905ba697ac1bbd2c
chayadeaver <chayadeaver@gmail.com>
2021-01-12 16:14:56 -0500

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_secure_password
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
