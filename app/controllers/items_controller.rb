649370babc913930c5afe7d04368096a317a5147
chayadeaver <chayadeaver@gmail.com>
2021-01-13 15:39:33 -0500

    add id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :price, :description, :image_url, :menu)
+      params.require(:item).permit(:name, :price, :description, :image_url, :menu_id)
     end
 end
